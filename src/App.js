a257d6cceec149e1a9553aba19c60322735e6a90
dermawansil <dermawansilaen19@gmail.com>
2022-03-19 00:23:39 +0700

    add fiture

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <h1>Hello derma</h1>
+        <h2>New Fitur </h2>
         <a
           className="App-link"
           href="https://reactjs.org"
